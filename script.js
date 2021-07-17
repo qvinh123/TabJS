d37a6b4eac73d5ca96f971afa77e4bb07f5bbe67
qvinh123 <60300240+qvinh123@users.noreply.github.com>
2021-07-17 23:29:59 +0700

    fix

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const tabsContent = document.querySelectorAll('.operations__content');
 
 tabsContainer.addEventListener("click", function (e) {
     const clicked = e.target.closest(".operations__tab")
-    // console.log(clicked.dataset)
+   
     if (!clicked) return;
 
     // Remove active classes
